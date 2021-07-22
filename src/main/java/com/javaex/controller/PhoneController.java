023b2a9ed032eaec775d7e382852d146e734c028
junzzan9 <ojs2681@gmail.com>
2021-07-22 16:31:19 +0900

    07/22 spring phonebook3 updateForm,update,delete

--- src/main/java/com/javaex/controller/PhoneController.java
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -1,10 +1,14 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.PhoneDao;
 import com.javaex.vo.PersonVo;
@@ -21,8 +25,17 @@ public class PhoneController {
 
 	// List
 	@RequestMapping(value = "/list", method = { RequestMethod.GET, RequestMethod.POST })
-	public String list() {
+	public String list(Model model) {
 		System.out.println("[PhoneController.list]");
+
+		PhoneDao pDao = new PhoneDao();
+
+		List<PersonVo> pList = pDao.getPersonList();
+
+		System.out.println(pList);
+
+		// model에 담기 (박스에담기)
+		model.addAttribute("pList", pList); // controller --> dispatcherservlet 전달
 
 		return "/WEB-INF/views/list.jsp"; // DispatcherServlet --> list.jsp로 foward
 	}
@@ -55,7 +68,7 @@ public class PhoneController {
 	 * return "/WEB-INF/views/list.jsp"; }
 	 */
 
-	// ModelAttribte 사용
+	// WRITE -- ModelAttribte 사용
 	@RequestMapping(value = "/write", method = { RequestMethod.GET, RequestMethod.POST })
 	public String write(@ModelAttribute PersonVo pVo) {
 		System.out.println("[PhoneController.write]");
@@ -65,9 +78,11 @@ public class PhoneController {
 
 		System.out.println(pVo);
 
+		// Dao 생성(메소드이용)
 		PhoneDao pDao = new PhoneDao();
 		pDao.personInsert(pVo);
 
+		// view -->리다이렉트
 		return "redirect:/list";
 	}
 
@@ -89,6 +104,47 @@ public class PhoneController {
 	 * return "/WEB-INF/views/list.jsp"; }
 	 */
 
+	// 삭제
+	@RequestMapping(value = "delete", method = { RequestMethod.GET, RequestMethod.POST })
+	public String delete(@RequestParam("no") int pId) {
+		System.out.println("[PhoneController.Delete]");
+
+		// Dao 생성(메소드이용)
+		PhoneDao pDao = new PhoneDao();
+		pDao.personDelete(pId);
+
+		// view -->리다이렉트
+		return "redirect:/list";
+
+	}
+
+	// 수정폼
+	@RequestMapping(value = "updateForm", method = { RequestMethod.GET, RequestMethod.POST })
+	public String updateForm(Model model, @RequestParam("no") int pId) {
+		System.out.println("[PhoneController.uForm]");
+		
+		
+		PhoneDao pDao = new PhoneDao();
+		PersonVo pVo = pDao.getPerson(pId);
+
+		model.addAttribute("pVo", pVo);
+
+		return "/WEB-INF/views/updateForm.jsp";
+	}
+	// 수정
+	@RequestMapping(value = "update", method = { RequestMethod.GET, RequestMethod.POST })
+	public String update(@ModelAttribute PersonVo pVo) {
+		System.out.println("[PhoneController.Update]");
+		
+		PhoneDao pDao = new PhoneDao();
+		pDao.personUpdate(pVo);
+		
+		return "redirect:/list";
+	}
+	
+	// 그림,코드
+
+	/*************************************************************************/
 	@RequestMapping(value = "/board/read/{no}", method = { RequestMethod.GET, RequestMethod.POST })
 	public String read(@PathVariable("no") int boardNo) {
 		System.out.println("[PhoneController.read]");
--- webapp/WEB-INF/views/list.jsp
+++ webapp/WEB-INF/views/list.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
 <!DOCTYPE html>
 <html>
@@ -10,45 +10,31 @@
 <body>
 	<h1>전화번호 리스트</h1>
 	<p>입력한 정보 내역입니다</p>
-
+	
+	
+	<c:forEach items="${pList }" var="pVo">
 	<table border="1">
 		<tr>
 			<td>이름</td>
-			<td>정우성</td>
+			<td>${pVo.name }</td>
 		</tr>
 		<tr>
 			<td>핸드폰</td>
-			<td>010-1111-1111</td>
+			<td>${pVo.hp }</td>
 		</tr>
 		<tr>
 			<td>회사</td>
-			<td>02-222-2222</td>
+			<td>${pVo.company }</td>
 		</tr>
 		<tr>
-			<td><a href="">[수정]</a></td>
-			<td><a href="">[삭제]</a></td>
+			<td><a href="/phonebook3/updateForm?no=${pVo.personId} ">[수정]</a></td>
+			<td><a href="/phonebook3/delete?no=${pVo.personId } ">[삭제]</a></td>
 		</tr>
 	</table>
 	<br>
-
-	<table border="1">
-		<tr>
-			<td>이름</td>
-			<td>정우성</td>
-		</tr>
-		<tr>
-			<td>핸드폰</td>
-			<td>010-1111-1111</td>
-		</tr>
-		<tr>
-			<td>회사</td>
-			<td>02-222-2222</td>
-		</tr>
-		<tr>
-			<td><a href="">[수정]</a></td>
-			<td><a href="">[삭제]</a></td>
-		</tr>
-	</table>
-	<br>
+	</c:forEach>
+	
+	<a href="/phonebook3/writeForm">[추가번호 등록]</a>
+	
 </body>
 </html>
